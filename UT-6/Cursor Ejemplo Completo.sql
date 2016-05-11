283cc84a156ed7237edbe7fbacf3bb863c53c55f
David Jimenez Gonzalez <davidjimenezgonza@gmail.com>
2016-05-11 14:17:44 +0200

    Update

--- UT-6/Cursor Ejemplo Completo.sql
+++ UT-6/Cursor Ejemplo Completo.sql
@@ -12,13 +12,13 @@ begin
     declare totalPedidosPorEstado int(11);
     declare cont int(11) default 0;
     
-	declare cursorPedidos cursor for select CodigoPedido, Estado from pedidos where codigoCliente = codCliente;
-	
+    declare cursorPedidos cursor for select CodigoPedido, Estado from pedidos where codigoCliente = codCliente;
+    
     declare continue handler for not found set fincursor = 1;
     
     create temporary table IF NOT EXISTS tTemp (
-		estado varchar(255),
-		codigoPedido varchar(255),
+        estado varchar(255),
+        codigoPedido varchar(255),
         codigoCliente varchar(255)
     );
     
@@ -26,49 +26,51 @@ begin
     
     wCursor : while fincursor != 1 do
     
-		fetch cursorPedidos into codPed, estadoPedido;
+        fetch cursorPedidos into codPed, estadoPedido;
         
         case 
         when estadoPedido = "entregado" then
-			
+            
             set totalPedidosPorEstado = (select count(*) from pedidos where estado = "entregado");
             
             wContador : while cont < totalPedidosPorEstado do
             
-				insert into tTemp select "entregado", CodigoPedido, codigoCliente from pedidos where estado = "entregado" limit cont, 1;
-				set cont = cont + 1;
+                insert into tTemp select "entregado", CodigoPedido, codigoCliente from pedidos where estado = "entregado" limit cont, 1;
+                set cont = cont + 1;
             
             end while wContador;
             
-		when estadoPedido = "Pendiente" then
+        when estadoPedido = "Pendiente" then
         
-			set totalPedidosPorEstado = (select count(*) from pedidos where estado = "pendiente");
+            set totalPedidosPorEstado = (select count(*) from pedidos where estado = "pendiente");
             
             wContador : while cont < totalPedidosPorEstado do
             
-				insert into tTemp select "pendiente", CodigoPedido, codigoCliente from pedidos where estado = "pendiente" limit cont, 1;
-				set cont = cont + 1;
+                insert into tTemp select "pendiente", CodigoPedido, codigoCliente from pedidos where estado = "pendiente" limit cont, 1;
+                set cont = cont + 1;
             
             end while wContador;
         
         when estadoPedido = "Servido" then
         
-			set totalPedidosPorEstado = (select count(*) from pedidos where estado = "Servido");
+            set totalPedidosPorEstado = (select count(*) from pedidos where estado = "Servido");
             
             wContador : while cont < totalPedidosPorEstado do
             
-				insert into tTemp select "Servido", CodigoPedido, codigoCliente from pedidos where estado = "Servido" limit cont, 1;
-				set cont = cont + 1;
+                insert into tTemp select "Servido", CodigoPedido, codigoCliente from pedidos where estado = "Servido" limit cont, 1;
+                set cont = cont + 1;
             
             end while wContador;
         
         else
         
-			select "Estado incorrecto de pedidos";
+            select "Estado incorrecto de pedidos";
             
-		end case;
+        end case;
         
     end while wCursor;
+    
+    close cursorPedidos;
     
     select * from tTemp;
 
